f7fed627cb5a3793dc0eecca9e82444dff17f51e
Maheshchoda <choda.maheshbabu@gmail.com>
2019-01-15 17:03:37 +0530

    Updates

---
+++ precache-manifest.9e00837313d01466d10782917c61b56c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "90af5016538b21a8d044e18ad8f0cd6d",
+    "url": "/ReportListing/static/media/spinner.90af5016.gif"
+  },
+  {
+    "revision": "7219175f0f022de291e709df0b39f4d9",
+    "url": "/ReportListing/static/media/mordorintelligence.7219175f.jpg"
+  },
+  {
+    "revision": "2eabd8cfa9f1c4def674",
+    "url": "/ReportListing/static/js/runtime~main.2eabd8cf.js"
+  },
+  {
+    "revision": "060bf275aa239a865d13",
+    "url": "/ReportListing/static/js/main.060bf275.chunk.js"
+  },
+  {
+    "revision": "72ee5458cd78a1f90a3e",
+    "url": "/ReportListing/static/js/1.72ee5458.chunk.js"
+  },
+  {
+    "revision": "060bf275aa239a865d13",
+    "url": "/ReportListing/static/css/main.eadba19c.chunk.css"
+  },
+  {
+    "revision": "7424a24910ca48e7eefd0884a3c22944",
+    "url": "/ReportListing/index.html"
+  }
+];
