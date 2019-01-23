eb8a8a96925418d30171b2112c82b24c14d9b293
Maheshchoda <choda.maheshbabu@gmail.com>
2019-01-23 22:02:11 +0530

    Updates

--- precache-manifest.45e806dea3dd6efcfa0097f7cab6732c.js
+++ precache-manifest.45e806dea3dd6efcfa0097f7cab6732c.js
@@ -4,27 +4,27 @@ self.__precacheManifest = [
     "url": "/ReportListing/static/media/spinner.90af5016.gif"
   },
   {
-    "revision": "7219175f0f022de291e709df0b39f4d9",
-    "url": "/ReportListing/static/media/mordorintelligence.7219175f.jpg"
+    "revision": "68019ee809f55d0f087fcc69c2ad9865",
+    "url": "/ReportListing/static/media/mordorintelligence.68019ee8.jpg"
   },
   {
     "revision": "2eabd8cfa9f1c4def674",
     "url": "/ReportListing/static/js/runtime~main.2eabd8cf.js"
   },
   {
-    "revision": "978ea175f7c8604359c5",
-    "url": "/ReportListing/static/js/main.978ea175.chunk.js"
+    "revision": "6c84ca5a08091b3aac22",
+    "url": "/ReportListing/static/js/main.6c84ca5a.chunk.js"
   },
   {
     "revision": "72ee5458cd78a1f90a3e",
     "url": "/ReportListing/static/js/1.72ee5458.chunk.js"
   },
   {
-    "revision": "978ea175f7c8604359c5",
+    "revision": "6c84ca5a08091b3aac22",
     "url": "/ReportListing/static/css/main.eadba19c.chunk.css"
   },
   {
-    "revision": "eb4b953fcee727f53989697d1588aa2a",
+    "revision": "7bf74d734bc85d2e4267697be6986a32",
     "url": "/ReportListing/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ReportListing/precache-manifest.8c5f8fb2a6b1f0bcd8ce07de248a18f9.js"
+  "/ReportListing/precache-manifest.45e806dea3dd6efcfa0097f7cab6732c.js"
 );
 
 workbox.clientsClaim();
